ebe41e39322cc1d367ae2b891a56e7fcbf8bafa9
Chinmay Srivastava <61424879+srivastavachinmay@users.noreply.github.com>
2021-10-30 02:31:52 +0530

    socket emit

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,29 @@ import express from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 
-import { logger } from './utils'
+import { logger } from './utils/utils'
 
 const app = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer, { /* options */ })
 
+app.get('/', (req, res) => { res.send("Hello World"); });
+
 io.on('connection', (socket) => {
-    logger.info(socket)
-})
+    socket.broadcast.emit('hi');
+    
+    socket.on('chat message', (msg) => {
+        logger.info('message: ' + msg);
+    });
+    socket.on('broadcast message', (msg) => {
+        io.emit('broadcast message', msg);
+        logger.info('bro message: ' + msg);
+
+    });
+
+
+
+});
 
 const start = () => {
     const port = process.env.SV_PORT || 3000
